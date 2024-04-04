26fc5f9da6e6b8d349cfa312b4d262f137826c3c
YuliaBas <y@basalygina.ru>
2024-04-04 17:41:29 +0200

    Sprint 9. SearchActivity commit

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,9 +2,11 @@ package com.example.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity(){
@@ -16,7 +18,31 @@ class SettingsActivity : AppCompatActivity(){
         buttonBack.setOnClickListener {
             onBackPressed()
         }
+        val share = findViewById<TextView>(R.id.share_app)
+        share.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.app_link))
+            shareIntent.type = "text/plain"
+            startActivity(Intent.createChooser(shareIntent, "Поделиться приложением"))
+        }
 
+        val contactSupport = findViewById<TextView>(R.id.contact_support)
+        contactSupport.setOnClickListener{
+            val contactSupportIntent = Intent(Intent.ACTION_SENDTO)
+            contactSupportIntent.data = Uri.parse("mailto:")
+            contactSupportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.default_email)))
+            contactSupportIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_default_subject))
+            contactSupportIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.email_default_text))
+            startActivity(contactSupportIntent)
+        }
+
+        val termsOfUse = findViewById<TextView>(R.id.terms_of_use)
+        termsOfUse.setOnClickListener{
+        val termsOfUseIntent = Intent(Intent.ACTION_VIEW)
+            termsOfUseIntent.data = Uri.parse(getString(R.string.terms_of_use_link))
+            termsOfUseIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(termsOfUseIntent)
+        }
     }
 
 
